37bf66d1db4c3518332a4f702fdf677b2652ab67
Tristin Xie <tristinxie@gmail.com>
2024-06-19 19:51:08 -0700

    Particle filter running from single image

--- ros_nodes/particle_filter.py
+++ ros_nodes/particle_filter.py
@@ -2,8 +2,8 @@
 import numpy as np
 
 class ParticleFilter:
-    def __init__(self, num_states, init_distribution, motion_model, obs_model, num_particles=1000, min_num_effective_particles=0.5):
-        self._particles = np.zeros((num_particles, ))
+    def __init__(self, num_states, init_distribution, motion_model, obs_model, num_particles, min_num_effective_particles=0.5):
+        self._particles = np.zeros((num_particles, num_states))
         self._weights = np.ones((num_particles))/float(num_particles)
 
         self._init_distribution = init_distribution
@@ -13,23 +13,20 @@ class ParticleFilter:
     def norm_weights(self):
         self._weights = self._weights/np.sum(self._weights)
 
-    def init_filter(self, std_r, std_t):
-        # self._omega_r = np.random.normal(0, std_r, size=(self._num_particles, 1, 4))
-        # self._omega_t = np.random.normal(0, std_t, size=(self._num_particles, 3))
+    def init_filter(self, std):
+        for p_idx, _ in enumerate(self._particles):
+             self._particles[p_idx], _ = self._init_distribution(std)
 
-        for p_idx, _ in enumerate(self._particle):
-             self._particles[p_idx], _ = self._init_distribution(self._std)
-
-    def predict(self):
+    def predict(self, std):
         for p_idx, particle in enumerate(self._particles):
-                self._particles[p_idx], _ = self._motion_model(particle, self._std)
+                self._particles[p_idx], _ = self._motion_model(particle, std)
 
 
     def update(self, points_2d, ctrnet, joint_angles, cam, cTr, gamma):
         for p_idx, particle in enumerate(self._particles):
              obs_prob = self._obs_model(particle, points_2d, ctrnet, joint_angles, cam, cTr, gamma)
              self._weights[p_idx] = self._weights[p_idx]*obs_prob
-        
+        print(self._weights)
         self.norm_weights()
 
         # TODO: resample if particles are depleted
--- ros_nodes/probability_funcs.py
+++ ros_nodes/probability_funcs.py
@@ -1,7 +1,10 @@
 import numpy as np
 import kornia
+import torch
+import cv2
 from scipy import optimize
 from scipy.stats import norm
+from sklearn.metrics.pairwise import rbf_kernel
 
 
 def sample_gaussian(std):
@@ -15,20 +18,49 @@ def additive_gaussian(state, std):
     sample, prob = sample_gaussian(std)
     return state + sample, prob
 
+def axisAngleToRotationMatrix(axis_angle):
+    angle = np.linalg.norm(axis_angle)
+    
+    if angle < 1e-5:
+        return np.eye(3)
+    
+    axis  = axis_angle/angle
+    cross_product_mat_axis = np.array([[0, -axis[2], axis[1]],
+                                       [axis[2], 0, -axis[0]],
+                                       [-axis[1], axis[0], 0]])
+    return np.cos(angle)*np.eye(3) + np.sin(angle) * cross_product_mat_axis \
+            + (1.0 - np.cos(angle))*np.outer(axis,axis)
 # What is robot_arm.getPointFeatures()
+# State is: [ori_x, ori_y, ori_z, pos_x, pos_y, pos_z]
 def point_feature_obs(state, points_2d, ctrnet, joint_angles, cam, cTr, gamma):
-
-    T = kornia.geometry.conversions.angle_axis_to_rotation_matrix(state)
-
-
+    #convert state to angle axis
+    T = np.eye(4)
+    # print(state.shape)
+    T[:-1, -1] = np.array(state[3:])
+    T[:-1, :-1] = axisAngleToRotationMatrix(state[:3])
     # Want to have final result of project image plane coords
     _, t_list = ctrnet.robot.get_joint_RT(joint_angles)
     p_t = t_list[[0,2,3,4,6,7,8]] # remove 1 and 5 links as they are overlapping with 2 and 6
-    K = ctrnet.intrinsics
+    K = np.float64(ctrnet.intrinsics)
     # only projects from camera coords to image coords
-    p_c = None
-    projected_points = cam.projectPoints(p_c)
+    # p_c = None
+    # projected_points = cam.projectPoints(p_c)
+    cTr = cTr.cpu().detach().numpy().squeeze()
+    # print(cTr.shape)
+    cTr_mat = np.eye(4)
+    cTr_mat[:-1, -1] = np.array(cTr[3:])
+    cTr_mat[:-1, :-1] = axisAngleToRotationMatrix(cTr[:3])
+    p_cTr  = np.dot(cTr_mat, T)
+    # print(p_cTr)
+    rvec = np.float64(p_cTr[:3, :3])
+    tvec = np.float64(p_cTr[:3, 3:])
+    # print(rvec)
+    # print(tvec)
+    projected_points, _ = cv2.projectPoints(p_t, rvec, tvec, K, None)
 
     # Make association between detected and projected points to compute prob and use prob to update weights
-    prob = 1
+    projected_points = projected_points.squeeze(1).reshape(1, 14)
+    detected_points = points_2d.reshape(1, 14).cpu().detach().numpy()
+    # print(projected_points, detected_points)
+    prob = rbf_kernel(projected_points, Y=detected_points).squeeze()
     return prob
